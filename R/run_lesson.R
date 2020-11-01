a7323191bafd4e2da86e6037cb6d8b746e82ddca
gaalcaras <gaby.alcaras@gmail.com>
2020-11-01 17:56:00 +0100

    R: document run_lesson()

--- R/run_lesson.R
+++ R/run_lesson.R
@@ -1,3 +1,9 @@
+#' Lancer un tutoriel de "socstat"
+#'
+#' @param lesson_name Le nom de la leçon
+#'
+#' @examples run_lesson("intro")
+#'
 #' @export
 run_lesson <- function(lesson_name) {
   learnr::run_tutorial(lesson_name,
